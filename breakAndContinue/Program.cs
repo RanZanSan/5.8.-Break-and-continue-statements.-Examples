deec9d5e74d70d3b8c5bf7eac933789dce416d70
Andrey Rasskazov <ranzansan@gmail.com>
2021-06-23 12:16:53 +0300

    Добавьте файлы проекта.

---
+++ breakAndContinue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 10; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    continue;
+                }
+                if (n == 7)
+                {
+                    break;
+                }
+
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ breakAndContinueToo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakAndContinueToo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 10)
+            {
+                if (n % 2 == 0)
+                {
+                    n++;
+                    continue;
+                }
+                if (n == 7)
+                {
+                    break;
+                }
+
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
